dc3db3ba36e781af225deffd59059eae2ff474f9
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-03-21 23:46:27 +0900

    Update revise.py

--- face_voice/revise.py
+++ face_voice/revise.py
@@ -41,43 +41,43 @@ def recognition_rate(text, place, tae_eon, myung_hyun):
 # 이름 인식 코드    
 def speak_jetson():
             
-        # 음성인식 객체 생성
-        r = sr.Recognizer()
+    # 음성인식 객체 생성
+    r = sr.Recognizer()
             
-        with sr.Microphone() as source :
+    with sr.Microphone() as source :
                 
-            # 마이크로부터 오디오 읽기
-            print('"젯슨"을 불러주세요!')
-            audio_data = r.record(source, duration = 3)
+        # 마이크로부터 오디오 읽기
+        print('"젯슨"을 불러주세요!')
+        audio_data = r.record(source, duration = 3)
                 
-            try:
-                # 구글 API로 인식 (하루에 50회 제한)
-                text = r.recognize_google(audio_data, language = 'ko')
+        try:
+            # 구글 API로 인식 (하루에 50회 제한)
+            text = r.recognize_google(audio_data, language = 'ko')
             
-                # 음성인식 시 오류나는 단어
-                jetson = ["잭슨", "넥슨", "넥센"]
+            # 음성인식 시 오류나는 단어
+            jetson = ["잭슨", "넥슨", "넥센"]
             
-                # 오류나는 젯슨 단어를 젯슨으로 바꿔주는 코드
-                for i in jetson :
-                    if i in text :
-                        text = text.replace(i, '젯슨')
+            # 오류나는 젯슨 단어를 젯슨으로 바꿔주는 코드
+            for i in jetson :
+                if i in text :
+                    text = text.replace(i, '젯슨')
 
-                # 이름 인식 -> 음성 인식 코드로 넘어감
-                if(text == "젯슨") :
-                    print("네! 부르셨나요?")
-                    txt = "네! 부르셨나요?"
-                    tts_kr = gTTS(txt, lang = 'ko', slow = False)
-                    tts_kr.save("voice.mp3")
-                    playsound.playsound("voice.mp3")
-                    return respeak()
+            # 이름 인식 -> 음성 인식 코드로 넘어감
+            if(text == "젯슨") :
+                print("네! 부르셨나요?")
+                txt = "네! 부르셨나요?"
+                tts_kr = gTTS(txt, lang = 'ko', slow = False)
+                tts_kr.save("voice.mp3")
+                playsound.playsound("voice.mp3")
+                return respeak()
                     
-                # 다른 단어 인식 -> 다시 이름 부르는 코드로 돌아감
-                else:
-                    return speak_jetson()
-            
-            # 음성 인식 실패한 경우
-            except sr.UnknownValueError:
+            # 다른 단어 인식 -> 다시 이름 부르는 코드로 돌아감
+            else:
                 return speak_jetson()
+            
+        # 음성 인식 실패한 경우
+        except sr.UnknownValueError:
+            return speak_jetson()
 
 # 음성 인식    
 def respeak():
