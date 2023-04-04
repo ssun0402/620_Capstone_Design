82fcdda68587d22ac0d600bc0105898277b2cda9
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2023-04-04 16:09:29 +0900

    Update thread.py

--- face_voice/thread.py
+++ face_voice/thread.py
@@ -3,35 +3,30 @@ import time
 from multi_voice import get_r_name_list
 import os
 from multi_face import Facerecognition
-
-lock = threading.Lock()
-
 # 스레드 테스트를 위해 def 2개 생성
-def func1():
-    with lock:
-        fr_instance = Facerecognition()
-        fr_instance.video()
-        time.sleep(1)
-        pass
+def func1(add):
+    face_recognition = Facerecognition()
+    time1 = 0
+    for names in face_recognition.video() :
+        if time1 == 15 :
+           time1 += 1
+           print(names)
 
-def func2():
-    with lock:
-        r_name_list = get_r_name_list()
-        time.sleep(1)
-        pass
+def func2(add):
+    r_name_list = get_r_name_list()
+    time.sleep(1)
 
 def main():
     #스레드 정의
-    thread1 = threading.Thread(target=func1)
-    thread2 = threading.Thread(target=func2)
+    thread1 = threading.Thread(target=func2, args=('1',))
+    thread2 = threading.Thread(target=func1, args=('2',))
 
     #스레드 시작
     thread1.start()
     thread2.start()
-    
-    thread1.join()
-    thread2.join()
     print("done!")
 
+    thread1.join()
+    thread2.join()
 if __name__ == "__main__":
     main()
